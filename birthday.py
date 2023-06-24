37f782a7d1c45419613baceb5662b37323f47f5e
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2023-06-24 17:28:27 +0300

    edit

--- birthday.py
+++
@@ -1,8 +0,0 @@
-def get_birthdays_per_week(name, birthday):
-
-
-
-
-
-if __name__ == "__main__":
-    main()
---
+++ birthday_folder/birthday.py
@@ -0,0 +1,18 @@
+def get_birthdays_per_week(name, birthday):
+
+user_dict={'name':'Rostyslav Bysko', 'birthday': '25-04-1974',
+Sergey Korchuk, 25-07-1960,
+Alexandr Korchuk, 20-10-1984,
+Viktor Bakharev, 23-06-1962,
+Svetlana Vratskaya, 24-06-1974,
+Fedor Bulko, 26-06-1980,
+Srgey Gudilin, 29-02-1972,
+Vladimir Metelev, 31-12-1982,
+Dmitry Kryvsha, 01-01-1988,
+Leonid Chayka, 26-06-1949,
+}
+
+
+
+if __name__ == "__main__":
+    main()
